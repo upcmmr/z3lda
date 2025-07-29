54a9f36bb735bedae8eae78577429b93763cbd7d
Nathan <nathan.flickerbox@gmail.com>
2025-07-28 19:15:35 -0700

    structure fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         <title>Z3lda - Sprite Command Game</title>
         <meta name="description" content="An interactive sprite command game" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <SpriteCommandGame />
     </>
